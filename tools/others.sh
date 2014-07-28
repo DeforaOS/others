6fd157cb819d09c98b947d9c61066ff752dfdc37
Pierre Pronchery <khorben@defora.org>
2014-07-28 21:28:49 +0200

    Updated the copyright line

--- tools/others.sh
+++ tools/others.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2011-2013 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2011-2014 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Unix others
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
