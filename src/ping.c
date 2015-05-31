84f625ca9a705b7b6898351cd0e167df7ff70fe4
Pierre Pronchery <khorben@defora.org>
2015-05-31 23:00:38 +0200

    Introduce support for IPv6

--- src/ping.c
+++ src/ping.c
@@ -41,6 +41,7 @@
 /* types */
 typedef struct _Prefs
 {
+	int family;
 	unsigned int count;
 } Prefs;
 #define PREFS_c	0x1
@@ -58,12 +59,10 @@ static int _ping_usage(void);
 /* ping */
 static int _ping(Prefs * prefs, char const * hostname)
 {
-	const int family = AF_INET;
 	struct addrinfo hints;
 	struct addrinfo * ai;
 	int res;
-	struct addrinfo * p;
-	struct sockaddr_in to;
+	struct addrinfo * to;
 	struct
 	{
 		struct
@@ -91,31 +90,32 @@ static int _ping(Prefs * prefs, char const * hostname)
 
 	/* lookup hostname */
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = family;
+	hints.ai_family = prefs->family;
 	hints.ai_socktype = SOCK_STREAM;
 	if((res = getaddrinfo(hostname, NULL, &hints, &ai)) != 0)
 		return _ping_gaierror(hostname, res);
-	for(p = ai; p != NULL; p = p->ai_next)
-		if(p->ai_family == AF_INET)
-		{
-			to = *(struct sockaddr_in *)p->ai_addr;
+	for(to = ai; to != NULL; to = to->ai_next)
+		if(to->ai_family == AF_INET || to->ai_family == AF_INET6)
 			break;
-		}
-	freeaddrinfo(ai);
-	if(p == NULL)
+	if(to == NULL)
 	{
 		fprintf(stderr, "%s: %s: Host not found\n", PROGNAME, hostname);
+		freeaddrinfo(ai);
 		return -1;
 	}
 	/* create the socket */
-	if((fd = socket(family, SOCK_RAW, IPPROTO_ICMP)) < 0)
+	if((fd = socket(to->ai_family, SOCK_RAW, IPPROTO_ICMP)) < 0)
+	{
+		freeaddrinfo(ai);
 		return _ping_error("socket", 1);
+	}
 	/* initialize the packet */
 	memset(&msg, 0, sizeof(msg));
 	msg.icmp.icmp_type = ICMP_ECHO;
 	printf("PING %s (%s): %lu data bytes\n", hostname,
-			inet_ntoa(to.sin_addr),
-			sizeof(msg));
+			(to->ai_family == AF_INET) ? inet_ntoa(
+				((struct sockaddr_in *)to->ai_addr)->sin_addr)
+			: "unknown", sizeof(msg));
 	for(i = 0; prefs->count == 0 || i < prefs->count; i++)
 	{
 		if(gettimeofday(&tv, NULL) != 0)
@@ -127,8 +127,8 @@ static int _ping(Prefs * prefs, char const * hostname)
 		msg.ts.tv_sec = htonl(tv.tv_sec);
 		msg.ts.tv_nsec = htonl(tv.tv_usec);
 		/* FIXME set the checksum */
-		if(sendto(fd, &msg, sizeof(msg), 0, (struct sockaddr *)&to,
-					sizeof(to)) != sizeof(msg))
+		if(sendto(fd, &msg, sizeof(msg), 0, to->ai_addr, to->ai_addrlen)
+				!= sizeof(msg))
 			cnt_errors += _ping_error(hostname, 1);
 		else
 			cnt_sent++;
@@ -137,6 +137,7 @@ static int _ping(Prefs * prefs, char const * hostname)
 	}
 	printf("%u packets transmitted, %u received, %u errors\n", cnt_sent,
 			cnt_received, cnt_errors);
+	freeaddrinfo(ai);
 	if(close(fd) != 0)
 		return _ping_error("close", 1);
 	return 0;
@@ -164,7 +165,7 @@ static int _ping_gaierror(char const * message, int error)
 /* ping_usage */
 static int _ping_usage(void)
 {
-	fputs("Usage: " PROGNAME " [-c count] hostname\n", stderr);
+	fputs("Usage: " PROGNAME " [-46][-c count] hostname\n", stderr);
 	return 1;
 }
 
@@ -179,10 +180,17 @@ int main(int argc, char * argv[])
 	char * p;
 
 	memset(&prefs, 0, sizeof(prefs));
+	prefs.family = AF_UNSPEC;
 	prefs.count = 4;
-	while((o = getopt(argc, argv, "c:")) != -1)
+	while((o = getopt(argc, argv, "46c:")) != -1)
 		switch(o)
 		{
+			case '4':
+				prefs.family = AF_INET;
+				break;
+			case '6':
+				prefs.family = AF_INET6;
+				break;
 			case 'c':
 				prefs.count = strtoul(optarg, &p, 0);
 				if(optarg[0] == '\0' || *p != '\0')
